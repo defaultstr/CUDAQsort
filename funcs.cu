8991956d1dac241dc19f131210f987eee8942c01
Near <near@near-ThinkPad-SL410.(none)>
2012-05-26 16:48:23 +0800

    the source of gpusort lib

--- funcs.cu
+++ funcs.cu
@@ -1,5 +1,19 @@
 #include "gpusort.h"
 
+/* parameters */
+/* dparams : the blocks
+darray1 : the first array1
+darray2 : the first array2
+flip : if true, use darray1, else use darray2 */
 __global__ void
 GQSORT(param *dparams, int *darray1, int *darray2, bool flip) {
+}
+
+/* parameters */
+/* dparams : the blocks
+darray1 : the first array1
+darray2 : the first array2
+flip : if true, use darray1, else use darray2 */
+__global__ void
+LQSORT(param *dlqparams, int *darray1, int *drray2, bool flip) {
 }
